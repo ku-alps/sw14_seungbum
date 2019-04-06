08dfbde41d7f638c7abd9ab5e5c8036e55e9b6fc
sbchun <38881981+sbchun@users.noreply.github.com>
2019-04-06 22:36:05 +0900

    Create Digital_Number.cpp

---
+++ Digital_Number.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int inputNum ;
+
+	cin >> inputNum;
+
+
+
+	while(inputNum/10){
+		int sum = 0;
+
+		while(inputNum%10){
+		sum += inputNum % 10;
+		inputNum /= 10;
+		}
+
+	inputNum = sum;
+	
+	}
+	
+	
+	cout << inputNum << endl;
+
+	return 0;
+}
